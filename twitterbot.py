4b3184d3545eb35685fffadc5722f5540eb837ad
argunner <argunner@users.noreply.github.com>
2015-01-25 00:45:59 +0530

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -13,10 +13,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-filename=open(argfile,'r')
-f = filename.readlines()
-filename.close()
- 
-for line in f:
-    api.update_status(line)
-    time.sleep(900) #This tweets every 15 minutes 
+with open(argfile,'r') as f:
+ for line in f.readlines():
+  api.update_status(line)
+  time.sleep(900) #This tweets every 15 minutes 
